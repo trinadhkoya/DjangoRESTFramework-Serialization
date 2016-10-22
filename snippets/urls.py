b754b256b94d2df99d6833a88ad3ef4bdb2c53a5
trinadhkoya <trinadhkoya9@gmail.com>
2016-10-22 10:16:36 +0530

    pk has been chnaged to key

--- snippets/urls.py
+++ snippets/urls.py
@@ -5,5 +5,5 @@ from snippets import views
 urlpatterns = [
     url(r'^snippets/$', views.snippet_list),
 
-    url(r'^snippets/(?P<pk>[0-9]+)/$', views.snippet_detail),
+    url(r'^snippets/(?P<key>[0-9]+)/$', views.snippet_detail),
 ]
